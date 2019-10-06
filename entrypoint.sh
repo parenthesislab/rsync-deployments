857318fdc38549534035a970c5535c71a1a66b4b
Elvis Morales <elvismdev@gmail.com>
2019-10-05 20:34:34 -0700

    Moving Linode deployment code to this repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,8 @@ echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
 
-# Do deployment
-# sh -c "rsync $1 -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $2 $GITHUB_WORKSPACE/ $3"
+# Move code to build folder
+sh -c "rsync -avz --exclude-from rsync-exclude.txt --delete --delete-excluded . $GITHUB_WORKSPACE/container_build"
+
+# Deploy to Linode Server
+sh -c "rsync -atu -e 'ssh -p 5956 -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' --progress --delete --exclude=/_btcpayserver $GITHUB_WORKSPACE/container_build/. $LINODE_SRV_USER@$LINODE_SRV_HOST:$LINODE_SRV_PATH"
